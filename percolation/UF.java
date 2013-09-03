ec7f48e70cb4dbe51c237f378531d9e5e249dd1e
gremdavel <MGD1981@gmail.com>
2013-09-03 02:45:06 -0400

    Finished main functions.

--- percolation/UF.java
+++ percolation/UF.java
@@ -1,23 +1,43 @@
 public class UF {
-    public int[] uf_array;
+    public int[] id;
+    public int[] sz;
     
     public UF(int[] array) {
-        uf_array = new int[array.length + 2];
-        for (int i : uf_array) {
-            uf_array[i] = i;
+        id = new int[array.length + 2]; // size+1 is top, +2 is bottom 
+        for (int i : id) {
+            id[i] = i;
+        }
+        sz = new int[id.length];
+        for (int i : sz) {
+            id[i] = 1;
         }
     }
 
-    public void union(int p, int q) {
-
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
     }
 
-    public void find(int p, int q) {
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
 
+    public boolean find(int p, int q) {
+       return root(p) == root(q); 
     }
 
     public boolean connected(int p, int q) {
-        return true; // TODO
+        return id[p] == id[q]; // TODO
     }
 
     public void count(int p) {
