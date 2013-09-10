ddd0b3e2b4630b017c98aacb9bfd2312c8c53502
gremdavel <MGD1981@gmail.com>
2013-09-10 10:07:21 -0400

    Almost working!

--- percolation/MonteCarlo.java
+++ percolation/MonteCarlo.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 public class MonteCarlo {
     private static Scanner scanner = new Scanner(System.in);
@@ -14,13 +15,16 @@ public class MonteCarlo {
         n = scanner.nextInt(); 
         grid = new Percolation(n);
         while (grid.percolates() == false) {
-            i = generator.nextInt();
-            j = generator.nextInt();
+            System.out.println("Does not yet percolate.");
+            i = generator.nextInt(grid.grid_size) + 1;
+            j = generator.nextInt(grid.grid_size) + 1;
             if (grid.isOpen(i, j) == false) {
                 grid.open(i, j);
                 open_sites++;
+                System.out.println("Opening " + i + ", " + j);
             }
         }
+        System.out.println(Arrays.toString(grid.grid_array));
         System.out.println(open_sites);
     }
 }
--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,26 +1,54 @@
+import java.util.Arrays;
 
 public class Percolation {
     public int grid_size;
     public int[] grid_array;
-    public QuickFindUF uf_grid;    
+    public UF uf_grid;    
 
     public Percolation(int N) { // create N-by-N grid, with all sites blocked
         grid_size = N;
         grid_array = new int[N*N];
-        for (int square : grid_array) {
+        for (int square=0; square < grid_array.length; square++) {
             grid_array[square] = 1; // 1 is blocked, 0 is open
         }
-        uf_grid = new QuickFindUF(N*N+2);
+        uf_grid = new UF(N*N+2);
+        for (int x=0; x < grid_size; x++) {
+            System.out.println("Connecting " + N*N + " to " + x);
+            System.out.println("Connecting " + (N*N+1) + " to " + (N*N-1-x));
+            uf_grid.union(N*N, x);
+            uf_grid.union(N*N+1, N*N-1-x);
+        }
         
     }
         
     public void open(int i, int j) { // open site (row i, column j) if it is 
                                      // not already
         this.checkBounds(i, j);
-        grid_array[(i * (grid_size-1)) + j] = 0; 
+        grid_array[(i * (grid_size-1)) + j] = 0;
+        if (i > 1) {
+            if (this.isOpen(i-1, j)) {
+                uf_grid.union((grid_size-1)*i + j - 1, grid_size*(i-1) + j - 1);
+            }
+        }
+        if (i < grid_size) {
+            if (this.isOpen(i+1, j)) {
+                uf_grid.union((grid_size-1)*i + j - 1, grid_size*(i+1) + j - 1);
+            }
+        }
+        if (j > 1) {
+            if (this.isOpen(i, j-1)) {
+                uf_grid.union((grid_size-1)*i + j - 1, grid_size*i + j - 2);
+            }
+        }
+        if (j < grid_size) {
+            if (this.isOpen(i, j+1)) {
+                uf_grid.union((grid_size-1)*i + j, grid_size*i + j);
+            }
+        }
     }
             
     public boolean isOpen(int i, int j) { // is site (row i, column j) open?
+        System.out.println("Checking " + i + ", " + j);
         this.checkBounds(i, j);       
         if (grid_array[(i * (grid_size-1)) + j] == 0) {
             return true;
@@ -39,8 +67,12 @@ public class Percolation {
     }
     
     public boolean percolates() { // does the system percolate?
+        System.out.println("Are " + grid_size*grid_size + " and " + 
+                           (grid_size*grid_size+1) + " connected?");
+        System.out.println(uf_grid.connected(
+            grid_size * grid_size, grid_size * grid_size + 1));
         return uf_grid.connected(
-            grid_size * grid_size + 1, grid_size * grid_size + 2);
+            grid_size * grid_size, grid_size * grid_size + 1);
     }
     
     private void checkBounds(int i, int j) {
